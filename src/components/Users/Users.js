08ea2e0ec2eb6bbf7ba05cddd610d9621a1e4fb7
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-26 19:37:48 +0800

    Add pagination for user component.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { connect } from 'dva'
+import { routerRedux } from 'dva/router'
 import { Table, Pagination, Popconfirm } from 'antd'
 import styles from './Users.css'
 import { PAGE_SIZE } from '../../constants'
 
-const Users = ({ list: dataSource, total, page: current, loading }) => {
+const Users = ({ dispatch, list: dataSource, total, page: current, loading }) => {
   const deleteHandler = (id) => {
     console.warn(`TODO: ${id}`)
+  }
+
+  const pageChangeHandler = (page) => {
+    dispatch(routerRedux.push({
+      pathname: 'users',
+      query: { page }
+    }))
   }
 
   const columns = [
@@ -55,6 +63,7 @@ const Users = ({ list: dataSource, total, page: current, loading }) => {
           total={total}
           current={current}
           pageSize={PAGE_SIZE}
+          onChange={pageChangeHandler}
         />
       </div>
     </div>
