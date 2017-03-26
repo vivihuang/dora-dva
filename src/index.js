eda33233d9234866c9adff0ab97c9d659f8727dd
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-26 19:34:53 +0800

    Import dva-loading to show loading animation.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import dva from 'dva';
-import './index.css';
+import dva from 'dva'
+import createLoading from 'dva-loading'
+import './index.css'
 
 // 1. Initialize
-const app = dva();
+const app = dva()
 
-app.model(require("./models/users"));
+app.model(require('./models/users'))
 
 // 2. Plugins
-// app.use({});
+// app.use({})
+app.use(createLoading())
 
 // 3. Model
-// app.model(require('./models/example'));
+// app.model(require('./models/example'))
 
 // 4. Router
-app.router(require('./router'));
+app.router(require('./router'))
 
 // 5. Start
-app.start('#root');
+app.start('#root')
