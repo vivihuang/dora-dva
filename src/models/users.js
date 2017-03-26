48267bc9407739d169198ed78330c66261d3e2da
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-26 19:52:01 +0800

    Add edit user handler.

--- src/models/users.js
+++ src/models/users.js
@@ -28,6 +28,11 @@ export default {
       yield call(usersService.remove, id)
       const page = yield select(state => state.users.page)
       yield put({ type: 'fetch', payload: { page } })
+    },
+    *patch({ payload: { id, values } }, { call, put, select }) {
+      yield call(usersService.patch, id, values)
+      const page = yield select(state => state.users.page)
+      yield put({ type: 'fetch', payload: { page } })
     }
   },
   subscriptions: {
