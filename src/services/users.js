18d09983416036755b3739a1d50bbb5b7fd09818
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-27 15:18:05 +0800

    Config proxy to redirect api request to mock server.

--- src/services/users.js
+++ src/services/users.js
@@ -2,7 +2,7 @@ import request from '../utils/request'
 import { PAGE_SIZE } from '../constants'
 
 export function fetch({ page }) {
-  return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`)
+  return request(`/api/users?page=${page}&limit=${PAGE_SIZE}`)
 }
 
 export function remove(id) {
