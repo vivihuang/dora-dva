475ddcd535ffcf6302f67ba7ddb530d304dc143a
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-28 15:42:01 +0800

    Add basic Login component.

---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Login.css'
+
+const Login = () => (
+  <div className={styles.normal}>
+    Component: Login
+  </div>
+)
+
+export default Login
---
+++ src/routes/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Login.css'
+
+import LoginComponent from '../components/Login/Login'
+
+const Login = () => (
+  <div className={styles.normal}>
+    <LoginComponent />
+  </div>
+)
+
+export default Login
