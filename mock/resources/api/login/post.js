6edc698ef33b8113e9fda2e311390768064f963c
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-29 15:11:04 +0800

    Check login email and password in mock server.

--- mock/resources/api/login/post.js
+++ mock/resources/api/login/post.js
@@ -1,9 +1,12 @@
+const adminEmail = 'admin@admin.com'
+const adminPassword = 'admin'
+
 module.exports = (req, res, next) => {
   const data = JSON.parse(req.body)
-  if (data.email && data.password) {
+  if (data.email === adminEmail && data.password === adminPassword) {
     res.send(200, { token: 111 })
   } else {
-    res.send(404)
+    res.send(401)
   }
   next()
 }
