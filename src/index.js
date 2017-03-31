ed53c1818bffa50a226a5ddcd860ccc752dff4bd
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-31 11:43:34 +0800

    Refactor index.js.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ import authModel from './models/auth'
 
 // 1. Initialize
 const app = dva()
-app.model(usersModel)
-app.model(authModel)
 
 // 2. Plugins
 // app.use({})
 app.use(createLoading())
 
 // 3. Model
-// app.model(require('./models/example'))
+app.model(usersModel)
+app.model(authModel)
 
 // 4. Router
 app.router(routers)
